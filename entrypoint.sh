92be3e609ddbf09961ad93c0e6dc787093ccb5e2
Marcos Bento <Marcos.Bento@ecmwf.int>
2024-10-07 07:59:21 +0100

    Add Dockerfile for Ubuntu 24.04

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e # Exit immediately if a command exits with a non-zero status.
+set -u # Treat unset variables as an error when substituting.
+set -x # Print commands and their arguments as they are executed.
+
+exec "$@"
